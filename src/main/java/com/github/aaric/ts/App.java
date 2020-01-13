e6baefd5f4b1c52370b3900aa990e1125d69846b
aaric <qlhan@incarcloud.com>
2020-01-13 16:05:15 +0800

    0.0.1-SNAPSHOT init

---
+++ src/main/java/com/github/aaric/ts/App.java
@@ -0,0 +1,23 @@
+package com.github.aaric.ts;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Spring Boot启动类
+ *
+ * @author Aaric, created on 2020-01-13T15:39.
+ * @version 0.0.1-SNAPSHOT
+ */
+@SpringBootApplication
+public class App {
+
+    /**
+     * Main
+     *
+     * @param args 命令行参数
+     */
+    public static void main(String[] args) {
+        SpringApplication.run(App.class, args);
+    }
+}
